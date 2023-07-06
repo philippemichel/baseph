dea41787da2e314b63f9caef253126fbed3d09cd
Philippe MICHEL <philippe@docteur-michel.fr>
2023-07-06 13:56:44 +0200

    tab1ph réécrit avec gtsummary.

--- R/tab12ph.R
+++ R/tab12ph.R
@@ -12,103 +12,26 @@
 #'
 #' @import dplyr
 #' @import gtsummary
-#' @import kableExtra
 #' @import labelled
-#' @import WriteXLS
+#' 
 #' @return tableau
 #' @export
 #'
-#' @examples tab1ph(dfx = patients, test = "med", note = "Tableau 1", lt = FALSE, export = FALSE)
+#' @examples tab1ph(dfx = patients, colx = 2:5, test = "zz")
 #'
-tab1ph  <- function(dfx,  test = "moy", note ="", lt = FALSE, export = FALSE){
-  #
-  if (test == "moy"){
-    note <- "n (%) - moyenne ± écart type"
+tab1ph <- function(dfx, colx = NULL, test = "med") {
+  if (test == "med"){
+    pp <- list(all_continuous() ~ "{median} ({p25}, {p75})", all_categorical() ~ "{n} ({p}%)")
+  } else {
+    pp <- list(all_continuous() ~ "{mean} ± {sd}", all_categorical() ~ "{n} ({p}%)")
   }
-  else{
-    note <- "n (%) - médiane (quartiles)"
-  }
-  #
-  tabf <- NULL
-  tlig <- 1
-  nl <- 1
-  #
-  for (l in 1:ncol(dfx)) {
-    varx <- dfx[l]
-    varx <- varx[,1]
-    lvarx <- length(na.omit(varx))
-    nom <- var_label(varx)
-    nom <- names(dfx)[l]
-    #
-    # Factoriel
-    #
-    if (!is.numeric(varx)) {
-      tabp <- c(nom,lvarx,"")
-      bb <- table(varx)
-      aa <- prop.table(bb)*100
-      for (i in 1:length(levels(varx))) {
-        ll <- c(levels(varx)[i],
-                "",
-                paste0(bb[i]," (",round(aa[[1]],1),"%)")
-        )
-        tabp <- rbind(tabp,ll)
-      }
-#      print(tabp)
-      nl <- nl + length(levels(varx))
-    }
-    else{
-      # Numérique
-      if (test == "moy"){
-        am <- signif(mean(varx, na.rm = TRUE),4)
-        as <- signif(sd(varx, na.rm = TRUE),4)
-        aa <- paste0(am, " ± ", as)
-      }
-      else{
-        mdx <- quantile(varx)
-        aa <- paste0(mdx[[3]], " (",mdx[[2]]," ; ",mdx[[4]],")")
-      }
-      tabp <- c(nom,lvarx,aa)
-    }
-    
-    tabf <- rbind(tabf,tabp)
-    #print(tabf)
-    nl <- nl + 1
-    tlig <- c(tlig,nl)
-  }
-  #
-  titn = c(" ","n", "n (%)")
-  #
-  # Export
-  #
-  if (export == TRUE) {
-    txls <-  as.data.frame(rbind(titn, tabf) )
-    extit <- paste0("tables/", tit, ".xls")
-    WriteXLS(
-      txls, extit
-    )
-  }
-  #
-  # Tracé
-  #
-  titn = c(" ","n", note)
-  tabf <- as_tibble(tabf)
-  ltab <- 1:nrow(tabf)
-  lg <- ltab %in% tlig
-  tlig <- tlig[-length(tlig)]
-  trait <- tlig[-1] - 1
-  tabf$V1 <- cell_spec(tabf$V1 , bold =  lg)
-  kbl(
-    tabf,
-    booktabs = TRUE,
-    longtable = lt,
-    row.names = FALSE,
-    col.names = titn,
-    escape = FALSE
-  ) |>
-    kable_styling(
-      latex_options = c("HOLD_position", "scale_down", "repeat_header"),
-      position = "center"
-    ) |>
-    footnote(general = note) |>
-    row_spec(trait, hline_after = TRUE)
+  if (is.null(colx)) {colx = 1:ncol(dfx)}
+  dfx |>
+    dplyr::select(colx) |>
+    tbl_summary(missing = "no",
+                statistic = pp) |>
+    modify_header(label ~ " ") |>
+    bold_labels() |>
+    add_n()
 }
+
