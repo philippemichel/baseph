68cf8fff25f33d6afb9111e2a52320d818e5af99
Dr Philippe MICHEL <philippe@docteur-michel.fr>
2026-05-07 16:05:26 +0200

    correction sur bicph

--- R/bicph.R
+++ R/bicph.R
@@ -1,5 +1,3 @@
-#' nbic
-#'
 #' Affiche un nombre avec son intervalle de confiance
 #'
 #' @param nn nb brut
@@ -12,9 +10,9 @@
 #'
 #' @examples bicph(nn = 55, bb = 22, hh = 77, pc = 3)
 bicph <- function(nn, bb, hh, pc = 3) {
-  nn <- signif(nn, 3)
-  bb <- signif(bb, 3)
-  hh <- signif(hh, 3)
+  nn <- signif(nn, pc)
+  bb <- signif(bb, pc)
+  hh <- signif(hh, pc)
   ll <- paste0(nn, " [", bb, " ; ", hh, "]")
   return(ll)
 }
